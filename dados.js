e0d11a8f167c22bd8112727b353dddea83f50a5f
João Pedro Gomes Barreto <joaopedrog062@gmail.com>
2026-05-18 17:20:36 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,8 +1,32 @@
 const ASSESSORES = [
-    { nome: 'Luis Siscato', retencao: '0,0%', retencaoV: 0, captacao: 'R$ 0M', receita: 'R$ 0K', pontos: 0, avColor: '#00d2ff' },
-    { nome: 'Erick Rodrigo', retencao: '0,0%', retencaoV: 0, captacao: 'R$ 0M', receita: 'R$ 0K', pontos: 0, avColor: '#b8b8b8' },
-    { nome: 'Pedro Aleixo', retencao: '0,0%', retencaoV: 0, captacao: 'R$ 0K', receita: 'R$ 0K', pontos: 0, avColor: '#cd7f32' },
-    { nome: 'Rafael Barbosa', retencao: '0,0%', retencaoV: 0, captacao: 'R$ 0M', receita: 'R$ 0K', pontos: 0, avColor: '#5577aa' },
-    { nome: 'Clérton Macambira', retencao: '0%', retencaoV: 0, captacao: 'R$ 0M', receita: 'R$ 0K', pontos: 0, avColor: '#5577aa' },
-    { nome: 'Sam Bastos', retencao: '0%', retencaoV: 0, captacao: 'R$ 0K', receita: 'R$ 0K', pontos: 0, avColor: '#5577aa' },
+    { 
+        nome: 'Luis Siscato', 
+        foto: 'img/Figurinha Luis.png', 
+        retencao: '0%', retencaoV: 0, captacao: 'R$ 0', receita: 'R$ 0', pontos: 0, avColor: '#00d2ff' 
+    },
+    { 
+        nome: 'Erick Rodrigo', 
+        foto: 'img/Figurinha Erick.png', 
+       retencao: '0%', retencaoV: 0, captacao: 'R$ 0', receita: 'R$ 0', pontos: 0, avColor: '#b8b8b8' 
+    },
+    { 
+        nome: 'Pedro Aleixo', 
+        foto: 'img/Figurinha Pedro.png', 
+       retencao: '0%', retencaoV: 0, captacao: 'R$ 0', receita: 'R$ 0', pontos: 0, avColor: '#cd7f32' 
+    },
+    { 
+        nome: 'Rafael Barbosa', 
+        foto: 'img/Figurinha Rafael.png', 
+       retencao: '0%', retencaoV: 0, captacao: 'R$ 0', receita: 'R$ 0', pontos: 0, avColor: '#5577aa' 
+    },
+    { 
+        nome: 'Clérton Macambira', 
+        foto: 'img/Figurinha Clerton.png', 
+        retencao: '0%', retencaoV: 0, captacao: 'R$ 0', receita: 'R$ 0', pontos: 0, avColor:'#5577aa' 
+    },
+    { 
+        nome: 'Sam Bastos', 
+        foto: 'img/Figurinha Sam.png', 
+       retencao: '0%', retencaoV: 0, captacao: 'R$ 0', receita: 'R$ 0', pontos: 0, avColor:'#5577aa' 
+    }
 ];
